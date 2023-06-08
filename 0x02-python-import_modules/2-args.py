f53e2b4b1b85bde727e0bde141ab2d6119be3ddd
Kevin-byt <myapps255@gmail.com>
2023-06-08 20:51:54 +0300

    infinitr addition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     import sys
 
     ele = len(sys.argv) - 1
-    if ele == 1:
+    if ele == 0:
+        print("{} arguments.".format(ele))
+    elif ele == 1:
         print("{} argument:".format(ele))
     else:
         print("{} arguments:".format(ele))
