ff1f81d9efb4564111e13aad68917629c27e740d
Kevin-byt <myapps255@gmail.com>
2023-06-08 20:10:21 +0300

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    ele = len(sys.argv) - 1
+    if ele == 1:
+        print("{} argument".format(ele))
+    else:
+        print("{} arguments".format(ele))
+
+    for a in range(1, len(sys.argv)):
+        print("{}: {}".format(a, sys.argv[a]))
