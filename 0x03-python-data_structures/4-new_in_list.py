e620077a3740558119e94087cb74e660ad0bf113
Kevin-byt <myapps255@gmail.com>
2023-06-11 20:18:25 +0300

    replace element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
-    
-    my_list[idx] = element
-    return my_list
+
+    new_list = [ele for ele in my_list]
+    new_list[idx] = element
+    return new_list
