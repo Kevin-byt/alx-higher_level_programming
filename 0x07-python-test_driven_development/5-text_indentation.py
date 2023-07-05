dd558054a6424b8d84e8d4a048be805b7dd263e5
Kevin-byt <myapps255@gmail.com>
2023-07-05 10:04:32 +0300

    python test driven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
-"""prints a text with 2 new lines after each of these characters: ., ? and :"""
+"""Print text with two new lines after each '.', '?', and ':'"""
 
 
 def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     characters = ['.', '?', ':']
     line = ""
     result = ""
+    c = 0
 
     for char in text:
         line += char
+        c += 1
+
+        if c == len(text):
+            line = line.strip()
+            result = result + line
+
         if char in characters:
             line = line.strip()
             result = result + line + '\n\n'
             line = ""
 
-    print(result, end="")
+    print(result)
