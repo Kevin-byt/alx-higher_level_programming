795a6fde6dc746303142009384cac5ba8dd0ad8a
Kevin-byt <myapps255@gmail.com>
2023-07-10 23:47:35 +0300

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
+"""contains the MyList class"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class.
-    Args:
-        list (int): A list object
-    """
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
